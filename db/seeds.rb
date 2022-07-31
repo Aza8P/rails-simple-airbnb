fd69ee5147677613bc295aaaa832df44a52a06d3
Ann Altantulga <a3jargal@icloud.com>
2022-07-31 19:35:35 +0800

    added all flats page & styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+london_flat = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+paris_flat = Flat.create!(
+  name: '2 rooms rue saint Maur',
+  address: '11th Paris Ile-de-France',
+  description: "Two rooms of 37m2 in the heart of the 11th arrondissement. Very quiet, overlooks courtyard, large bed (1,60x2m). Very cozy fully equipped kitchen. Decorating :)
+  Nice apartment 37m2 in a very nice place. Very quiet and comfy, I'm still in decoration process :)",
+  price_per_night: 72,
+  number_of_guests: 2
+)
+
+california_flat = Flat.create!(
+  name: 'Casa Mami Pioneertown',
+  address: 'Pioneertown, California, United States',
+  description: "The surrounding landscape and views are truly hard to describe. You can hike and explore the desert right outside our house.",
+  price_per_night: 292,
+  number_of_guests: 4
+)
+
+palm_flat = Flat.create!(
+  name: 'Shelby Sands - Private Resort-Like Pool Home',
+  address: 'Palm Springs, California, United States',
+  description: "Completely walled and hedged in for privacy, Shelby Sands is like checking in to your own private Palm Springs resort compound!",
+  price_per_night: 518,
+  number_of_guests: 6
+)
